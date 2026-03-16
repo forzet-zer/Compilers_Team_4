e2a77d8274e8a7038552bfef34d92e260e516a58
VictorEscDiaz <temmo375@gmail.com>
2026-03-15 18:14:01 -0600

    Improve lexer output and add token counter

--- Compilador/main.py
+++ Compilador/main.py
@@ -1,51 +1,72 @@
 from AnalizadorLexico import Lexer
 import sys
-from copy import copy, deepcopy
+from copy import copy
 import os.path
 
 
 print("\t\t\t\t*  Compiler for the language Juan Pato v0.1   *")
-print("\t\t\t\t------------------------------------------------------\n\n")
-print("\033[0m")
+print("\t\t\t\t------------------------------------------------------\n")
 
-print("\ninput the name of the source program (default: holamundo.jp) or 'exit' to exit:\n")
+print("\nInput the name of the source program (default: holamundo.jp) or 'exit' to exit:\n")
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
-src_file = sys.stdin.readline()
-src_file = src_file.rstrip()
+src_file = sys.stdin.readline().rstrip()
+
 if src_file == "":
     src_file = "holamundo.jp"
-elif  src_file == "exit":
-    print("\033[92m\nCompiler terminated.\n")
+elif src_file == "exit":
+    print("\nCompiler terminated.\n")
     exit()
 
 target_path = os.path.join(base_dir, "programas", src_file)
-print (target_path)
+
 if not os.path.isfile(target_path):
-  print("\nthe program \""+src_file+"\" does not exist.\033[92m\n\nCompiler terminated.\n")
-  exit()
+    print(f'\nThe program "{src_file}" does not exist.\nCompiler terminated.\n')
+    exit()
+
 programa_fuente = open(target_path, 'r', encoding='utf-8').read()
+
 etapa = 6
 while etapa < 1 or etapa > 5:
-    print("\n* Input the number of the stage up to which you would like to execute the compilation proccess (default: 1):\n")
+    print("\nSelect compilation stage:\n")
     print("(1) Lexical analysis\n")
+
     try:
-        etapa = sys.stdin.readline()
-        etapa = int(etapa.rstrip())
+        etapa = int(sys.stdin.readline().rstrip())
     except:
         etapa = 6
+
     if etapa == "":
         etapa = 1
+
+
 if etapa >= 1:
 
     lexer = Lexer().get_lexer()
-    print("\033[92m\n----------------------------------------------------")
-    print("Starting lexical analysis")
-    print("----------------------------------------------------\n\033[0m")
-    print("\nSource program:\n\033[96m")
-    print(programa_fuente)
-    print('\033[0m')
-    tokens = lexer.lex(programa_fuente)
-    print("\nIdentified tokens:\n\033[93m")
 
-    for token in copy(tokens):
-        print(token)
+    print("\n====================================================")
+    print("               LEXICAL ANALYSIS STARTED             ")
+    print("====================================================\n")
+
+    print("Source Program:\n")
+    print("------------------------------------------")
+    print(programa_fuente)
+    print("------------------------------------------\n")
+
+    tokens = list(lexer.lex(programa_fuente))
+
+    print("Tokens Identified:\n")
+
+    print("{:<20} {:<20}".format("TOKEN TYPE", "VALUE"))
+    print("-" * 40)
+
+    contador = 0
+
+    for token in tokens:
+        print("{:<20} {:<20}".format(token.name, token.value))
+        contador += 1
+
+    print("-" * 40)
+    print(f"Total tokens identified: {contador}\n")
+
+    print("Lexical analysis completed.\n")
